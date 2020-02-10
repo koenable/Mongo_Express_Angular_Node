0ba61453ca97ec96ad1d8db70b9243d0e8e447d9
Koenable <mokoenabaholo90@gmail.com>
2020-02-10 15:37:49 +0200

    Revert "to be deleted"
    This reverts commit c373202142b2df3f1f64beb55832fde0d7922b70.

--- MCC/src/app/app.module.ts
+++ MCC/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { FixturesComponent } from './fixtures/fixtures.component';
-import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    FixturesComponent,
-    NavComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

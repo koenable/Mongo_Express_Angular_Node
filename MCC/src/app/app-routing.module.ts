414e731b5d36ad447cb64b5c6b2ad9cf332970b9
Koenable <mokoenabaholo90@gmail.com>
2020-02-10 15:56:03 +0200

    Create a Home and Fixtures Components. I also added routing paths to both routes.

--- MCC/src/app/app-routing.module.ts
+++ MCC/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component'; // Add this
+import { FixturesComponent } from './fixtures/fixtures.component'; // Add this
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },              // Add this
+  { path: 'fixtures', component: FixturesComponent }           // Add this
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
